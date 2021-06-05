f0251ac6e5928a92448deefd65d802d3a0217678
Astariul <remondnicola@gmail.com>
2021-06-05 14:22:41 +0900

    :art: Clean RST off example

--- docs/conf.py
+++ docs/conf.py
@@ -155,17 +155,17 @@ html_theme = 'sphinx_rtd_theme'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = "img/swole.png"
+# html_logo = "img/swole.png"
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = 'img/doge.png'
+# html_favicon = 'img/doge.png'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 html_css_files = ['css/custom.css']
 
